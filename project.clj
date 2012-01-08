030cd5884adfab49a9bbd8756a1e9c3bb7d0c4d0
Michael S. Klishin <michael@novemberain.com>
2012-01-08 05:57:53 +0400

    AOT compile UrlLike

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "A tiny Clojure library that parses URIs, URLs and relative values that real world HTML may contain"
   :license     { :name "Eclipse Public License" }
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :warn-on-reflection true
   :source-path        "src/clojure"
-  :java-source-path   "src/java")
+  :java-source-path   "src/java"
+  :aot                [clojurewerkz.urly.UrlLike]
+  :warn-on-reflection true)
