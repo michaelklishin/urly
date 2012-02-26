0deb6ec6daa5ff91b03bb7779e0822700bee792f
Michael S. Klishin <michael@novemberain.com>
2012-02-27 02:53:20 +0400

    Add Guava dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject clojurewerkz/urly "1.0.0-SNAPSHOT"
   :description "A tiny Clojure library that parses URIs, URLs and relative values that real world HTML may contain"
   :license     { :name "Eclipse Public License" }
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure    "1.3.0"]
+                 [com.google.guava/guava "11.0.1"]]
   :source-path        "src/clojure"
   :java-source-path   "src/java"
   :warn-on-reflection true)
