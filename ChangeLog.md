d658bae0e5760694d12fd245414e45df989c1307
Michael S. Klishin <michael@novemberain.com>
2012-03-11 11:26:51 +0400

    Migrate to Leiningen 2

---
+++ ChangeLog.md
@@ -0,0 +1,5 @@
+## Changes between Urly 1.0.0-beta2 and 1.0.0-beta3
+
+### Leiningen 2
+
+Urly now uses [Leiningen 2](https://github.com/technomancy/leiningen/wiki/Upgrading).
--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
 (defproject clojurewerkz/urly "1.0.0-SNAPSHOT"
   :description "A tiny Clojure library that parses URIs, URLs and relative values that real world HTML may contain"
-  :license     { :name "Eclipse Public License" }
-  :dependencies [[org.clojure/clojure    "1.3.0"]
+  :min-lein-version "2.0.0"
+  :license {:name "Eclipse Public License"}
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.google.guava/guava "11.0.1"]]
-  :source-path        "src/clojure"
-  :java-source-path   "src/java"
+  :profiles {:all { :dependencies [[com.google.guava/guava "11.0.1"]] }
+             :1.4 { :dependencies [[org.clojure/clojure "1.3.0"]] }}
+  :source-paths ["src/clojure"]
+  :java-source-paths ["src/java"]
   :warn-on-reflection true)
