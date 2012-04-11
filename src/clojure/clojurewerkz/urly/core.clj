79d03fff83a00444285a1dbbe8ddf3c0625e9868
Michael S. Klishin <michael@novemberain.com>
2012-04-12 01:39:28 +0400

    Eliminate some reflection warnings, remove some dead code

--- src/clojure/clojurewerkz/urly/core.clj
+++ src/clojure/clojurewerkz/urly/core.clj
@@ -163,13 +163,10 @@
   (.resolve (URI. base) (.toURI ^URL other)))
 (defmethod resolve [UrlLike java.net.URL]
   [base other]
-  (url-like (.resolve (.toURI base) (.toURI ^URL other))))
-(defmethod resolve [UrlLike java.net.URL]
-  [base other]
-  (url-like (.resolve (.toURI ^UrlLike base) (.toURI ^URL other))))
+  (url-like (.resolve ^URI (.toURI ^UrlLike base) ^URI (.toURI ^URL other))))
 (defmethod resolve [UrlLike java.net.URI]
   [base other]
-  (url-like (.resolve (.toURI ^UrlLike base) other)))
+  (url-like (.resolve ^URI (.toURI ^UrlLike base) other)))
 (defmethod resolve [UrlLike UrlLike]
   [base other]
   (url-like (.resolve (.toURI ^UrlLike base) (.toURI ^UrlLike other))))
