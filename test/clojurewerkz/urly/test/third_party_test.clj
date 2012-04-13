6b48f8f5ae29fbb163e7c05e0b52896a0c1672e1
Michael S. Klishin <michael@novemberain.com>
2012-04-14 01:56:09 +0400

    A few more test cases

--- test/clojurewerkz/urly/test/third_party_test.clj
+++ test/clojurewerkz/urly/test/third_party_test.clj
@@ -13,12 +13,13 @@
 
 (deftest test-example-set1
   (are [input m] (is (= (as-map (url-like input)) m))
-       "//some_path"                       { :protocol nil,    :host nil,               :port -1, :user-info nil,       :path "/", :query nil, :fragment nil, :tld nil }
-       "HTTP://www.example.com/"           { :protocol "http", :host "www.example.com", :port -1, :user-info nil,       :path "/", :query nil, :fragment nil, :tld "com" }
-       "HtTP://www.EXAMPLE.com"            { :protocol "http", :host "www.example.com", :port -1, :user-info nil,       :path "/", :query nil, :fragment nil, :tld "com" }
-       "http://user:pw@www.ExAmPlE.com/"   { :protocol "http", :host "www.example.com", :port -1, :user-info "user:pw", :path "/", :query nil, :fragment nil, :tld "com" }
-       "http://USER:PW@www.example.com/"   { :protocol "http", :host "www.example.com", :port -1, :user-info "USER:PW", :path "/", :query nil, :fragment nil, :tld "com" }
-       "http://user@www.example.com/"      { :protocol "http", :host "www.example.com", :port -1, :user-info "user",    :path "/", :query nil, :fragment nil, :tld "com" }
-       "http://user%3Apw@www.ExAmPlE.com/" { :protocol "http", :host "www.example.com", :port -1, :user-info "user:pw", :path "/", :query nil, :fragment nil, :tld "com" }
-       "HTTP://X.COM/Y"                    { :protocol "http", :host "x.com",           :port -1, :user-info nil,       :path "/Y", :query nil, :fragment nil, :tld "com" }
-       "/foo/bar?baz=quux#frag"            { :protocol nil,    :host nil,               :port -1, :user-info nil,       :path "/foo/bar", :query "baz=quux", :fragment "frag", :tld nil }))
+       "//some_path"                       {:protocol nil,    :host nil,               :port -1, :user-info nil,       :path "/", :query nil, :fragment nil, :tld nil}
+       "HTTP://www.example.com/"           {:protocol "http", :host "www.example.com", :port -1, :user-info nil,       :path "/", :query nil, :fragment nil, :tld "com"}
+       "HtTP://www.EXAMPLE.com"            {:protocol "http", :host "www.example.com", :port -1, :user-info nil,       :path "/", :query nil, :fragment nil, :tld "com"}
+       "http://user:pw@www.ExAmPlE.com/"   {:protocol "http", :host "www.example.com", :port -1, :user-info "user:pw", :path "/", :query nil, :fragment nil, :tld "com"}
+       "http://USER:PW@www.example.com/"   {:protocol "http", :host "www.example.com", :port -1, :user-info "USER:PW", :path "/", :query nil, :fragment nil, :tld "com"}
+       "http://user@www.example.com/"      {:protocol "http", :host "www.example.com", :port -1, :user-info "user",    :path "/", :query nil, :fragment nil, :tld "com"}
+       "http://user%3Apw@www.ExAmPlE.com/" {:protocol "http", :host "www.example.com", :port -1, :user-info "user:pw", :path "/", :query nil, :fragment nil, :tld "com"}
+       "HTTP://X.COM/Y"                    {:protocol "http", :host "x.com",           :port -1, :user-info nil,       :path "/Y", :query nil, :fragment nil, :tld "com"}
+       "/foo/bar?baz=quux#frag"            {:protocol nil,    :host nil,               :port -1, :user-info nil,       :path "/foo/bar", :query "baz=quux", :fragment "frag", :tld nil}
+       "http:/foo/bar?baz=quux#frag"       {:protocol "http", :host nil,               :port -1, :user-info nil,       :path "/foo/bar", :query "baz=quux", :fragment "frag", :tld nil}))
